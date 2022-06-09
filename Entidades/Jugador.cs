4c38fd8e7d31657684b2f35b5708ad7727c9860f
Andrés Felipe Londoño Campos <78039311+ApidriuC@users.noreply.github.com>
2022-06-09 11:38:10 -0500

    Add files via upload

---
+++ Entidades/Jugador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Jugador
+    {
+        public string Nombre;
+        public int Puntaje;
+
+        public Jugador()
+        {
+        }
+        
+        public Jugador(string nombre, int puntaje)
+        {
+            Nombre = nombre;
+            Puntaje = puntaje;
+        }
+
+        public void AsignarNombre(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public string ObtenerNombre()
+        {
+            return Nombre;
+        }
+
+        public void AsignarPuntaje(int puntaje)
+        {
+            Puntaje = puntaje;
+        }
+        
+        public int ObtenerPuntaje()
+        {
+            return Puntaje;
+        }
+    }
+}
